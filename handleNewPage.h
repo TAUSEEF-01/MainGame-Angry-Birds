1dd7b719899fb2c8d364c213c48d18447a97aa21
TAUSEEF-01 <114896538+TAUSEEF-01@users.noreply.github.com>
2023-11-14 13:46:40 +0600

    Delete handleNewPage.h

--- handleNewPage.h
+++
@@ -1,8 +0,0 @@
-#ifndef HANDLE_NEW_PAGE
-#define HANDLE_NEW_PAGE
-
-#include "header.h"
-
-void handleNewPage(SDL_Renderer *renderer, SDL_Texture *newPageTexture, SDL_Texture *playButtonTexture, SDL_Rect playButtonRect, SDL_Texture *settingButtonTexture, SDL_Rect settingButtonRect, bool &quit, State &currentState);
-
-#endif
