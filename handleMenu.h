c4dd8a982f93cc0d035e08000a376c4c79cadeb0
TAUSEEF-01 <114896538+TAUSEEF-01@users.noreply.github.com>
2023-11-14 13:46:28 +0600

    Delete handleMenu.h

--- handleMenu.h
+++
@@ -1,8 +0,0 @@
-#ifndef HANDLE_MENU
-#define HANDLE_MENU
-
-#include "header.h"
-
-void handleMenu(SDL_Renderer *renderer, SDL_Texture *startButtonTexture, SDL_Rect startButtonRect, bool &quit, State &currentState);
-
-#endif
